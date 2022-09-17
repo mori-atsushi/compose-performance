785d9881acf8a3cc8f0e94210d4feda621335fbe
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-09-17 20:04:18 +0900

    Add ListSamle

--- app/src/main/kotlin/com/moriatsushi/performance/MainActivity.kt
+++ app/src/main/kotlin/com/moriatsushi/performance/MainActivity.kt
@@ -3,14 +3,17 @@ package com.moriatsushi.performance
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import com.moriatsushi.performance.list.List
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text(text = "Hello, World")
+            MaterialTheme {
+                List()
+            }
         }
     }
 }
